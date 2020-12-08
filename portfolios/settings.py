f247bbc4331289e497be33c180f700472cfda7ae
kilewa <kilewageorge230@gmail.com>
2020-12-08 13:16:49 +0300

    added crispy forms

--- portfolios/settings.py
+++ portfolios/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'portfolior',
     "bootstrap4",
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
